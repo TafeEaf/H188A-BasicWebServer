1915ee3d4debca1e957ee9ffd925fa0e8e74b827
IlGioMagnifico <ilgiomagnifico@gmail.com>
2018-08-21 10:15:50 +1000

    Add check preset

--- src/main.cpp
+++ src/main.cpp
@@ -17,36 +17,56 @@
 #define  BlueLedOn    "<font color=\"#7f7fff\"><b>ON</b></font>"
 #define  BlueLedOff   "<font color=\"#0000cc\"><b>OFF</b></font>"
 
-String GreenLedStatus, RedLedStatus, BlueLedStatus;
-
 // create Objects
 ESP8266WebServer server ( 80 );
 
 String getPage(){
-  String page = "<html lang=en-EN><head><meta http-equiv='refresh' content='10'/>";
+  String checkedOn, checkedOff;
+  String GreenLedStatus, RedLedStatus, BlueLedStatus;
+  //                                           Automatic refresh every 30 seconds
+  String page = "<html lang=en-EN><head><meta http-equiv='refresh' content='30'/>";
   page += "<title>ESP8266</title>";
   page += "<style> body { background-color: #fffff; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }</style>";
   page += "</head><body><h1>Wemos Wifi Basic Server</h1>";
   page += "<h3>Remote GPIO configuration</h3>";
   page += "<form action='/' method='POST'>";
+
   // handle Green LED
-  page += "<ul><li>Green Led (Current status: ";
+  if (digitalRead(GreenLed)==0){
+    GreenLedStatus = GreenLedOn; checkedOn = "checked"; checkedOff = "";
+  }
+  else{
+    GreenLedStatus = GreenLedOff; checkedOn = ""; checkedOff = "checked";
+  }
+  page += "<ul><li>Green Led Current status: ";
   page += GreenLedStatus;
-  page += ")";
-  page += "<INPUT type='radio' name='LED1' value='0'>ON";
-  page += "<INPUT type='radio' name='LED1' value='1'>OFF</li></ul>";
+  page += "<INPUT type='radio' name='LED1' value='0'"; page += checkedOn;  page += ">ON";
+  page += "<INPUT type='radio' name='LED1' value='1'"; page += checkedOff;  page += ">OFF</li></ul>";
+
   // handle Red LED
-  page += "<ul><li>Red Led (Current status: ";
+  if (digitalRead(RedLed)==0){
+     RedLedStatus = RedLedOn; checkedOn = "checked"; checkedOff = "";
+  }
+  else{
+     RedLedStatus = RedLedOff; checkedOn = ""; checkedOff = "checked";
+  }
+  page += "<ul><li>Red   Led Current status: ";
   page += RedLedStatus;
-  page += ")";
-  page += "<INPUT type='radio' name='LED2' value='0'>ON";
-  page += "<INPUT type='radio' name='LED2' value='1'>OFF</li></ul>";
+  page += "<INPUT type='radio' name='LED2' value='0'"; page += checkedOn;  page += ">ON";
+  page += "<INPUT type='radio' name='LED2' value='1'"; page += checkedOff;  page += ">OFF</li></ul>";
+
   // handle Blue LED
-  page += "<ul><li>Red Led (Current status: ";
+  if (digitalRead(BlueLed)==0){
+    BlueLedStatus = BlueLedOn; checkedOn = "checked"; checkedOff = "";
+  }
+  else{
+    BlueLedStatus = BlueLedOff; checkedOn = ""; checkedOff = "checked";
+  }
+  page += "<ul><li>Blue  Led Current status: ";
   page += BlueLedStatus;
-  page += ")";
-  page += "<INPUT type='radio' name='LED3' value='0'>ON";
-  page += "<INPUT type='radio' name='LED3' value='1'>OFF</li></ul>";
+  page += "<INPUT type='radio' name='LED3' value='0'"; page += checkedOn;  page += ">ON";
+  page += "<INPUT type='radio' name='LED3' value='1'"; page += checkedOff;  page += ">OFF</li></ul>";
+
   // send form
   page += "<INPUT type='submit' value='Send'>";
   page += "</body></html>";
@@ -55,50 +75,36 @@ String getPage(){
 
 
 void handleSubmit() {
-  String LED_1_Value = server.arg("LED1");
-  String LED_2_Value = server.arg("LED2");
-  String LED_3_Value = server.arg("LED3");
   // update Green LED
   Serial.print("Set Green Led: ");
-  if ( LED_1_Value == "0" ) {
+  if ( server.arg("LED1") == "0" )
     digitalWrite(GreenLed, 0);
-    GreenLedStatus = GreenLedOn;
-  } else if ( LED_1_Value == "1" ) {
+  else
     digitalWrite(GreenLed, 1);
-    GreenLedStatus = GreenLedOff;
-  } else {
-    Serial.print("No parameter sent, using previous: ");
-  }
   Serial.println(!digitalRead(GreenLed));
+
   // update RedLed LED
   Serial.print("Set Red Led: ");
-  if ( LED_2_Value == "0" ) {
+  if ( server.arg("LED2") == "0" )
     digitalWrite(RedLed, 0);
-    RedLedStatus = RedLedOn;
-  } else if ( LED_2_Value == "1" ) {
+  else
     digitalWrite(RedLed, 1);
-    RedLedStatus = RedLedOff;
-  } else {
-    Serial.print("No parameter sent, using previous: ");
-  }
   Serial.println(!digitalRead(RedLed));
+
   // update Blue LED
   Serial.print("Set Blue Led: ");
-  if ( LED_3_Value == "0" ) {
+  if ( server.arg("LED3") == "0" )
     digitalWrite(BlueLed, 0);
-    BlueLedStatus = BlueLedOn;
-  } else if ( LED_3_Value == "1" ) {
+  else
     digitalWrite(BlueLed, 1);
-    BlueLedStatus = BlueLedOff;
-  } else {
-    Serial.print("No parameter sent, using previous: ");
-  }
   Serial.println(!digitalRead(BlueLed));
 
+  //After changes resend the recompiled web page
   server.send ( 200, "text/html", getPage() );
 }
 
 void handleRoot(){
+  //               Green Led                  Red Led                  Blue Led
   if ( server.hasArg("LED1") || server.hasArg("LED2") || server.hasArg("LED3")) {
     handleSubmit();
   } else {
@@ -108,14 +114,13 @@ void handleRoot(){
 
 void setup() {
   pinMode(GreenLed, OUTPUT);            // sets the digital pin D2 as output
-  pinMode(RedLed, OUTPUT);              // sets the digital pin D3 as output
-  pinMode(BlueLed, OUTPUT);             // sets the digital pin D4 as output
   digitalWrite(GreenLed, 1);            // sets the digital pin D2 high
+
+  pinMode(RedLed, OUTPUT);              // sets the digital pin D3 as output
   digitalWrite(RedLed, 1);              // sets the digital pin D3 high
+
+  pinMode(BlueLed, OUTPUT);             // sets the digital pin D4 as output
   digitalWrite(BlueLed, 1);             // sets the digital pin D4 high
-  GreenLedStatus  =   GreenLedOff;
-  RedLedStatus    =   RedLedOff;
-  BlueLedStatus   =   BlueLedOff;
 
   Serial.begin ( 115200 );
 
